890f996a0816957039426261358e9a931e9a338c
jainakshat16 <akshat1273.cse19@chitkara.edu.in>
2020-06-27 12:16:01 +0530

    initial commit

---
+++ Pattern1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,k=1;
+    for(i=0;i<4;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+            printf("%d",k);
+            k++;
+
+
+        }
+    printf("\n");
+
+    }
+
+
+
+
+}
